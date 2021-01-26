876614807ae9e2ffb0992556446980d10e372211
Rob <rmuchall@protonmail.com>
2021-01-26 18:39:45 +0000

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 // core
 export * from "./MetaTransformer";
 // decorators
+export * from "./decorators/property/Exclude";
 export * from "./decorators/property/Transform";
+// enums
+export * from "./enums/DecoratorType";
 // interfaces
 export * from "./interfaces/ClassType";
 export * from "./interfaces/Metadata";
